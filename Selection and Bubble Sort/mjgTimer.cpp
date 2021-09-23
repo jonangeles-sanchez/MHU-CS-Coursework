9f7c0ab8f56bd06029afc24d266c0327e4a370ca
Jonathan Angeles <70487639+jonangeles-sanchez@users.noreply.github.com>
2021-09-23 12:36:26 -0400

    Create mjgTimer.cpp

---
+++ Selection and Bubble Sort/mjgTimer.cpp
@@ -0,0 +1,20 @@
+
+
+#include "mjgTimer.h"
+
+double getNow()
+{
+	struct timeval t;
+	double time;
+
+	gettimeofday(&t,NULL);
+	time = 0.000001* (t.tv_usec) + t.tv_sec;
+	return time;
+}
+
+double getElapsed(double *db)
+{
+	double now = getNow();
+	double temp = now - *db;
+	return temp;
+}
