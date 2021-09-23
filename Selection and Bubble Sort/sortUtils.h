b832d1ac53efb596b8db2abb026ead72b327e965
Jonathan Angeles <70487639+jonangeles-sanchez@users.noreply.github.com>
2021-09-23 12:39:02 -0400

    Create sortUtils.h

---
+++ Selection and Bubble Sort/sortUtils.h
@@ -0,0 +1,10 @@
+#ifndef _SORTUTILS_H
+#define _SORTUTILS_H
+
+#include "doublylinked.cpp"
+
+void selectionSort(List<int> &);
+void insertionSort(List<int> &);
+void loadRandomList(List<int> &);
+
+#endif
