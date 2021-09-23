86dfce7f88532b2dcd117b4602f269d5af96a6f9
Jonathan Angeles <70487639+jonangeles-sanchez@users.noreply.github.com>
2021-09-23 12:39:57 -0400

    Create unittest_sisort.cpp

---
+++ Selection and Bubble Sort/unittest_sisort.cpp
@@ -0,0 +1,208 @@
+#include <iostream>
+#include <cstdlib>
+#include "UnitTest++.h"
+#include "sortUtils.h"
+#include "mjgTimer.h"
+
+using namespace std;
+
+SUITE(ListTest){
+	class ListSetup {
+		public:
+			List<int> list1;
+			List<int> list2;
+
+			void loadList(int num, List<int> &list){
+
+				list.clear();
+
+				srand(time(NULL));
+				int randNum = 0;
+				for(int i = 0; i < num; i++){
+					randNum = rand() % 500;
+
+					if(list.full()) break;
+					Error_code result = list.insert(list.size(), randNum);
+					if(result != success){
+						cerr << "Error with list.insert()" << endl;
+						CHECK(result==success);
+					}
+				}
+
+			}
+
+			bool isSorted(List<int> &list){
+				int size = list.size();
+				if(size <= 1) return true;
+
+				int val1, val2;
+				list.retrieve(0, val1);
+				for(int i = 1; i < size; i++){
+					list.retrieve(i, val2);
+
+					if(val2 < val1){
+						return false;
+					}
+					val1 = val2;
+				}
+
+				return true;
+		
+			}
+
+	};
+
+	TEST_FIXTURE(ListSetup, TestSelectionSort){
+		list1.insert(0, 2);
+		list1.insert(1, 1);
+
+		selectionSort(list1);
+
+		CHECK_EQUAL(true, isSorted(list1));
+	}
+
+	TEST_FIXTURE(ListSetup, TestSelectionNearlySortedList){
+		for(int i = 0; i < 50; i++){
+			list1.insert(i, i);
+		}
+
+		list1.replace(0, 100);
+		selectionSort(list1);
+		CHECK_EQUAL(true, isSorted(list1));
+	}
+
+	TEST_FIXTURE(ListSetup, TestSelectionListUnchanged){
+		loadList(20, list1); 
+		list2 = list1;
+
+		selectionSort(list1);
+
+		CHECK_EQUAL(list2.size(), list1.size());
+		if(list1.size() == list2.size()) {
+
+			int temp, temp2;
+			for(int i = 0; i < list1.size(); i++){
+				list1.retrieve(i, temp);
+
+				bool found = false;
+				for(int j = 0; j < list2.size(); j++){
+					list2.retrieve(j, temp2);
+					if(temp == temp2){
+						list2.remove(j, temp2);
+						found = true;
+						break;
+					}
+				}
+
+				CHECK_EQUAL(true, found);
+			}
+
+			CHECK_EQUAL(0, list2.size());
+		}
+	}
+
+	TEST_FIXTURE(ListSetup, TestSelectionReverseSorted){
+		for(int i = 0; i < 50; i++){
+			list1.insert(i, 50 - i);
+		}
+
+		selectionSort(list1);
+		CHECK_EQUAL(true, isSorted(list1));
+	}
+
+	TEST_FIXTURE(ListSetup, TestSelectionLargeSort){
+		loadList(10000, list1);
+		CHECK_EQUAL(false, isSorted(list1));
+
+		//print a warning
+		cout << "This could take between 5-15 seconds...hang in there" << endl;
+
+		//see how long it takes
+		double timer = getNow();
+		selectionSort(list1);
+		timer = getElapsed(&timer);
+
+		cout << "Large selection sort took " << timer << " s" << endl;
+		CHECK_EQUAL(true, isSorted(list1));
+	}
+
+
+	TEST_FIXTURE(ListSetup, TestInsertionSort){
+		list1.insert(0, 2);
+		list1.insert(1, 1);
+
+		insertionSort(list1);
+
+		CHECK_EQUAL(true, isSorted(list1));
+	}
+
+	TEST_FIXTURE(ListSetup, TestInsertionNearlySortedList){
+		for(int i = 0; i < 50; i++){
+			list1.insert(i, i);
+		}
+
+		list1.replace(0, 100);
+		insertionSort(list1);
+		CHECK_EQUAL(true, isSorted(list1));
+	}
+
+	TEST_FIXTURE(ListSetup, TestInsertionListUnchanged){
+		loadList(20, list1); 
+		list2 = list1;
+
+		insertionSort(list1);
+
+		CHECK_EQUAL(list2.size(), list1.size());
+		if(list1.size() == list2.size()) {
+
+			int temp, temp2;
+			for(int i = 0; i < list1.size(); i++){
+				list1.retrieve(i, temp);
+
+				bool found = false;
+				for(int j = 0; j < list2.size(); j++){
+					list2.retrieve(j, temp2);
+					if(temp == temp2){
+						list2.remove(j, temp2);
+						found = true;
+						break;
+					}
+				}
+
+				CHECK_EQUAL(true, found);
+			}
+
+			CHECK_EQUAL(0, list2.size());
+		}
+	}
+
+
+	TEST_FIXTURE(ListSetup, TestInsertionReverseSorted){
+		for(int i = 0; i < 50; i++){
+			list1.insert(i, 50 - i);
+		}
+
+		insertionSort(list1);
+		CHECK_EQUAL(true, isSorted(list1));
+	}
+
+	TEST_FIXTURE(ListSetup, TestInsertionLargeSort){
+		loadList(10000, list1);
+		CHECK_EQUAL(false, isSorted(list1));
+
+		//print a warning
+		cout << "This could take between 5-15 seconds...hang in there" << endl;
+
+		//see how long it takes
+		double timer = getNow();
+		insertionSort(list1);
+		timer = getElapsed(&timer);
+
+		cout << "Large insertion sort took " << timer << " s" << endl;
+		CHECK_EQUAL(true, isSorted(list1));
+	}
+}
+
+int main(){
+	return UnitTest::RunAllTests();
+}
